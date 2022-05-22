5fafaa4a134f6f0a6dfe0bdd0baa8eb481a6a4bf
rahulcdev13 <rahulc.dev13@gmail.com>
2022-05-23 04:52:44 +0530

    Final commet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import Covid from './Covid-19-Live/Covid';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from "react-router-dom";
+import Routing from './Routing';
+import "./index.css";
 
 
 const App = () => {
-  return(
+  return (
     <>
-    <Covid />
-    {/* <h1>COVID-13 CORONAVIRUS TRACKER</h1> */}
+      <BrowserRouter>
+        <Routing />
+      </BrowserRouter>
+      {/* <Covid /> */}
+      {/* <h1>COVID-13 CORONAVIRUS TRACKER</h1> */}
+      {/* <br /><br /> */}
+      {/* <Statewise /> */}
     </>
   )
 }
